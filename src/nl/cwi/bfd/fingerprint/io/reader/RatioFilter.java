b910c79071300b04c118c64c35478eb6823f75c4
jahnestacado <ioannis.kiwe@gmail.com>
2013-04-19 01:07:46 +0200

    added RatioFilter class in order to differentiate betwwen input fragments while building fingerprints

---
+++ src/nl/cwi/bfd/fingerprint/io/reader/RatioFilter.java
@@ -0,0 +1,14 @@
+package nl.cwi.bfd.fingerprint.io.reader;
+
+public class RatioFilter {
+
+	public static boolean checkRatio(float minRatio, float maxRatio, String path){
+		String[] array = path.split("_");
+		float fragmentsRatio = Float.valueOf(array[3]); // 3 is the substring of the path that contains the ratio information
+		if(fragmentsRatio > minRatio && fragmentsRatio <= maxRatio)
+			return true;
+	
+		return false;
+	}
+
+}
